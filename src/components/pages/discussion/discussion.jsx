ca64223c5b8d9152a95548b703d56baba766696e
SilverSpear28 <abhijaymessi@gmail.com>
2020-02-27 21:30:20 +0530

    add condistional rendering

--- src/components/pages/discussion/discussion.jsx
+++ src/components/pages/discussion/discussion.jsx
@@ -4,8 +4,10 @@ class Discussion extends React.Component {
 
     constructor(props) {
         super(props);
+        this.onAskingQuestion = this.onAskingQuestion.bind(this);
         this.state = {
-            question: "Your Question"
+            question: "Your Question",
+            confirmation: false,
         }
     }
 
@@ -16,22 +18,52 @@ class Discussion extends React.Component {
         this.setState({ question: event.target.value });
     }
 
-    handleSubmit = (event) => {
-        alert('Do you want to proceed?');
-        event.preventDefault();
+    handleSubmit = () => {
+        var conf = confirm('Confirm!');
+        if (conf == true) { 
+            this.setState({confirmation: true});
+            // document.getElementById("UsersQuestion").innerHTML = this.props.question;
+        }
     }
 
+    
+    // componentDidUpdate() {
+    //     if(this.props.confirmation){
+    //         document.getElementById("UsersQuestion").innerHTML = this.props.question;
+    
+    //     }
+    // }
+    // handleOK = (event) => {
+    //     if(event== true){
+    //         this.state.confirmedQuestion = this.state.question;
+    //     }
+    // }
+
     render = () => {
+
+        const confirmation=this.state.confirmation;
+
         return (
+            <div>
             <form onSubmit={this.handleSubmit}>
                 <label>
                     <textarea value={this.state.question} onChange={this.onAskingQuestion} />
                 </label>
                 <button type="submit">Submit</button>
             </form>
+            <display confirmation={confirmation} />
+            </div>
         );
     }
 
 }
+
+function display(props) {
+    if(props.confirmation){
+    return <h1>HELLO from outside the class</h1>;
+    }
+}
+
+
 
 export default Discussion;
