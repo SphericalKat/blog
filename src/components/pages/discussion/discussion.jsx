b4091b8fc10f9107fba0641ef9a54a11e6af9631
SilverSpear28 <abhijaymessi@gmail.com>
2020-03-11 10:28:42 +0530

    de-bugged and tested discussion page

--- src/components/pages/discussion/discussion.jsx
+++ src/components/pages/discussion/discussion.jsx
@@ -1,69 +1,72 @@
 import React from "react";
 
 class Discussion extends React.Component {
-
+    
     constructor(props) {
         super(props);
-        this.onAskingQuestion = this.onAskingQuestion.bind(this);
-        this.state = {
-            question: "Your Question",
-            confirmation: false,
-        }
+        this.state = {text: '', inputText: '', mode:'view'};
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
-
-    // this.onAskingQuestion = this.onAskingQuestion.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-
-    onAskingQuestion = (event) => {
-        this.setState({ question: event.target.value });
+      
+    handleChange = (e) => {
+        this.setState({ inputText: e.target.value });
     }
-
-    handleSubmit = () => {
-        var conf = confirm('Confirm!');
-        if (conf == true) { 
-            this.setState({confirmation: true});
-            // document.getElementById("UsersQuestion").innerHTML = this.props.question;
-        }
+      
+    handleSave = () =>  {
+        this.setState({text: this.state.inputText, mode: 'view'});
     }
-
     
-    // componentDidUpdate() {
-    //     if(this.props.confirmation){
-    //         document.getElementById("UsersQuestion").innerHTML = this.props.question;
-    
-    //     }
-    // }
-    // handleOK = (event) => {
-    //     if(event== true){
-    //         this.state.confirmedQuestion = this.state.question;
-    //     }
-    // }
-
-    render = () => {
-
-        const confirmation=this.state.confirmation;
-
+    handleEdit = () => {
+        this.setState({mode: 'edit'});
+    }
+      
+    renderInputField = () => {
+        let input;
+        
+        if(this.state.mode !== 'view') {
+          input = 
+            <p>
+              <input
+                onChange={this.handleChange}
+                value={this.state.inputText} />
+            </p>;
+        }
+          
+        return input;
+    }
+      
+    renderButton = () => {
+        let button;
+        
+        if(this.state.mode === 'view') {
+          button =
+              <button onClick={this.handleEdit}>
+                Edit
+              </button>;
+        }
+        else {
+          button =
+              <button onClick={this.handleSave}>
+                Save
+              </button>;
+        }
+        
+        return button;
+    }
+      
+    render() {
         return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    <textarea value={this.state.question} onChange={this.onAskingQuestion} />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
-            <display confirmation={confirmation} />
-            </div>
+          <div>
+            <p>Text: {this.state.text}</p>
+            {this.renderInputField()}
+            {this.renderButton()}
+          </div>
         );
     }
-
 }
-
-function display(props) {
-    if(props.confirmation){
-    return <h1>HELLO from outside the class</h1>;
-    }
-}
-
 
 
 export default Discussion;
