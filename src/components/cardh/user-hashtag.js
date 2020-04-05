c3202208c9c74ac9601a898a3f2621398439c931
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-05 20:13:10 +0530

    Fix#1 user-hastag

--- src/components/cardh/user-hashtag.js
+++ src/components/cardh/user-hashtag.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 const Hashtag = (props) => {
+
+    let h = 
+        <div className='hash-div'>
+                <div className='hashes'>
+                    <div className='text'>
+                        #DEV {/*props.hash*/}
+                    </div>
+                </div>
+            </div>
+
     return(
         <div className='hashtag-box'>
             <div className='hash-head'>
                 Followed Hashtags
             </div>
-            <div className='hash-div'>
-                <div className='hashes'>
-                    <div className='text'>
-                        #DEV
-                    </div>
-                </div>
-            </div>
+            {h}
         </div>
     )
 }
