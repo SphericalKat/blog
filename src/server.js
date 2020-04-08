bfc831c0e6fefb554685332223ca3f29b691efb1
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-08 18:04:29 +0530

    Add createBlog.jsx

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import signup from './routes/signUp'
 import create from './routes/create'
 import blog from './routes/blog'
 import user from './routes/user'
+import createBlog from './routes/createBlog'
 import path from 'path'
 
 // Server var
@@ -30,6 +31,7 @@ app.use('/signup', signup)
 app.use('/create',create)
 app.use('/blog',blog)
 app.use('/user',user)
+app.use('/createBlog',createBlog)
 
 const port = process.env.PORT || 3000
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ const config = [{
     signUp: ['./src/components/entrypoints/signUp.jsx'],
     create: ['./src/components/entrypoints/create.jsx'],
     blog: ['./src/components/entrypoints/blog.jsx'],
-    user: ['./src/components/entrypoints/user.jsx']
+    user: ['./src/components/entrypoints/user.jsx'],
+    createBlog: ['./src/components/entrypoint/createBlog.jsx']
   },
   output: {
     path: path.resolve(__dirname, 'src', 'public', 'js'),
