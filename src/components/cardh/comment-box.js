a25847d9d60b3defcdffdaebb5a8c3e174d2836f
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-08 13:23:21 +0530

    Add user in comments

--- src/components/cardh/comment-box.js
+++ src/components/cardh/comment-box.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
 const CommentBox = (props) => {
-    return(
-                <div className='comment-box'>
-                    <div className='comments'>
-                        Hello whats up
+    return (
+        <div className='comment-box'>
+            <div style={{display: 'flex'}}>
+            <div className='comment-photo'>
+            </div>
+            <div className='user-comment'>
+                <div className='user-comment-name'>
+                    Shizuka
                     </div>
-                    <div className='comment-like'>
-                        <img className='func' src='http://localhost:3000/svg/thumbs-up.svg' />
-                        <img className='func' src='http://localhost:3000/svg/message-circle.svg' />
+                <div className='user-des'>
+                    Engineer
                     </div>
-                </div>
+            </div>
+            </div>
+            <div className='comments'>
+                Hello whats up
+                    </div>
+            <div className='comment-like'>
+                <img className='func' src='http://localhost:3000/svg/thumbs-up.svg' />
+                <img className='func' src='http://localhost:3000/svg/message-circle.svg' />
+            </div>
+        </div>
     )
 }
 
