fd465ecd57740e108cd76275d47010c1b11b4906
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-09 22:44:20 +0530

    props comment-box.js in card.js

--- src/components/cardh/card.js
+++ src/components/cardh/card.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import CommentContainer from './comment-container'
-import Comments from './comments'
 
 const hljs = require('highlight.js')
-    const markdown = require('markdown-it')({
-    html: true,
-    linkify: true,
-    typographer: false,
-    highlight: function (str, lang) {
+const markdown = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: false,
+  highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
         return hljs.highlight(lang, str).value
-      } catch (__) {}
+      } catch (__) { }
     }
 
     return '' // use external default escaping
@@ -20,20 +19,21 @@ const hljs = require('highlight.js')
 
 
 class Card extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
     this.card = {
-      head : 'HEAD', //Topic name needs to passed in here
-      tag : '#DEV', //Top tag ie #DEV
-      likes : 5, //Number of likes calculated dynamically
-      comments : 1, //Number of comments on the post
-      name : 'Shizuka', // Who posted the query
-      descriptionName : 'Enginner' //What Job description does he give, ex. FullStackDev 
+      head: 'HEAD', //Topic name needs to passed in here
+      tag: '#DEV', //Top tag ie #DEV
+      likes: 5, //Number of likes calculated dynamically
+      comments: 1, //Number of comments on the post
+      name: 'Shizuka', // Who posted the query
+      descriptionName: 'Enginner', //What Job description does he give, ex. FullStackDev 
+      photoLink: ''
     }
 
     this.markdownContent = markdown.render(props.content)
-    
+
 
   }
 
@@ -41,53 +41,53 @@ class Card extends React.Component {
     const markdownContent = this.markdownContent;
     return (
       <div className='card-tb'>
-      <div className='card-top'>
-        <div className='card-top-details'>
-          <div className='card-top-details-head'>
-            {this.card.head}
-          </div>
-          <div className='space' />
-          <div className='card-top-details-dev'>
-            {this.card.tag}
-          </div>
-        </div>
-      </div>
-      <div className='writeup'>
-        <div className='content markdown' dangerouslySetInnerHTML={{ __html: markdownContent }} />
-      </div>
-      <div className='read-more'>
-        <div className='button'>...Read More</div>
-      </div>
-      <div className='functions'>
-        <div className='like-box'>
-          <div className='like'>
-            <img src='http://localhost:3000/svg/thumbs-up.svg' />
-            <div className='i'>
-              {this.card.likes}
+        <div className='card-top'>
+          <div className='card-top-details'>
+            <div className='card-top-details-head'>
+              {this.card.head}
             </div>
-          </div>
-          <div className='like'>
-            <img src='http://localhost:3000/svg/message-circle.svg' alt='fuck you' />
-            <div className='i'>
-    {this.card.comments}
+            <div className='space' />
+            <div className='card-top-details-dev'>
+              {this.card.tag}
             </div>
           </div>
         </div>
-        <div className='head-card'>
-          <div className='photo-details'>
-            <div className='photo-name'>
-              {this.card.name}
+        <div className='writeup'>
+          <div className='content markdown' dangerouslySetInnerHTML={{ __html: markdownContent }} />
+        </div>
+        <div className='read-more'>
+          <div className='button'>...Read More</div>
+        </div>
+        <div className='functions'>
+          <div className='like-box'>
+            <div className='like'>
+              <img src='http://localhost:3000/svg/thumbs-up.svg' />
+              <div className='i'>
+                {this.card.likes}
+              </div>
             </div>
-            <div className='des'>
-              {this.card.descriptionName}
+            <div className='like'>
+              <img src='http://localhost:3000/svg/message-circle.svg' alt='fuck you' />
+              <div className='i'>
+                {this.card.comments}
+              </div>
             </div>
           </div>
-          <div className='photo' />
+          <div className='head-card'>
+            <div className='photo-details'>
+              <div className='photo-name'>
+                {this.card.name}
+              </div>
+              <div className='des'>
+                {this.card.descriptionName}
+              </div>
+            </div>
+            <img className='photo' src={this.photoLink} />
+          </div>
+          <div />
         </div>
-        <div />
+        <CommentContainer />
       </div>
-      <CommentContainer />
-    </div>
     )
   }
 
--- src/components/cardh/comment-box.js
+++ src/components/cardh/comment-box.js
@@ -1,28 +1,43 @@
 import React from 'react'
 
-const CommentBox = (props) => {
-  return (
-    <div className='comment-box'>
-      <div className='comment-box-header' style={{ display: 'flex' }}>
-        <div className='comment-photo' />
-        <div className='user-comment'>
-          <div className='user-comment-name'>
-                    Shizuka
+
+
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.commentCard = {
+      name : 'Shizuka',
+      descriptionUser : 'Engineer',
+      photo : '',
+      comment : ' HEllo'
+    }
+  }
+
+  render() {
+    return (
+      <div className='comment-box'>
+        <div className='comment-box-header' style={{ display: 'flex' }}>
+          <img className='comment-photo' />
+          <div className='user-comment'>
+            <div className='user-comment-name'>
+              {this.commentCard.name}
           </div>
-          <div className='user-des'>
-                    Engineer
+            <div className='user-des'>
+              {this.commentCard.descriptionUser}
+          </div>
           </div>
         </div>
+        <div className='comments'>
+          {this.commentCard.comment}
       </div>
-      <div className='comments'>
-                Hello whats up
+        <div className='comment-like'>
+          <img className='func' src='http://localhost:3000/svg/thumbs-up.svg' />
+          <img className='func' src='http://localhost:3000/svg/message-circle.svg' />
+        </div>
       </div>
-      <div className='comment-like'>
-        <img className='func' src='http://localhost:3000/svg/thumbs-up.svg' />
-        <img className='func' src='http://localhost:3000/svg/message-circle.svg' />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default CommentBox
