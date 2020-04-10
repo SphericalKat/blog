77a7f9e5ca6296befbb570e542d30c6cd0202fe0
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-10 23:14:56 +0530

    colors changed

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import blog from '../components/pages/blog/blog'
+import Blog from '../components/pages/blog/blog'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const reactComp = renderToString(<blog />)
-  res.status(200).render('pages/blog', { reactApp: reactComp, url: req.app.locals.url })
+  const reactComp = renderToString(<Blog />)
+  res.status(200).render('pages/Blog', { reactApp: reactComp, url: req.app.locals.url })
 })
 
 export default router
