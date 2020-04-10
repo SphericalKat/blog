69b564cf3c0775c33684a60ca6cdce6ee2e0edb5
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-04-10 18:47:01 +0530

    Initial Skeleton ready
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ src/components/preview/preview.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const HlJs = require('highlight.js')
+const markdown = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: false,
+  highlight: function (str, lang) {
+    if (lang && HlJs.getLanguage(lang)) {
+      try {
+        return HlJs.highlight(lang, str).value
+      } catch (__) {}
+    }
+
+    return '' // use external default escaping
+  }
+}).use(require('markdown-it-emoji'))
+
+class Preview extends React.Component {
+  render () {
+    const rendered = markdown.render(this.props.content)
+    return (
+      <div className='preview-container' style={{ width: '100%' }}>
+        <div className='preview' dangerouslySetInnerHTML={rendered} />
+      </div>
+    )
+  }
+}
+
+export default Preview
