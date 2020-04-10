4226a08c59782a1e559afbf6aeeca964560f95ca
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-04-10 23:02:58 +0530

    Fix side-drawer
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- src/components/sideDrawer/sideDrawer.js
+++ src/components/sideDrawer/sideDrawer.js
@@ -2,33 +2,51 @@ import React from 'react'
 import Hashes from '../cardh/user-hashtag'
 import Activity from '../cardh/activity'
 
-const sideDrawer = props => {
-  let drawerClasses = 'side-drawer'
-  if (props.show) {
-    drawerClasses = 'side-drawer open'
+class SideDrawer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.user = props.user
+
+    if (!this.user) {
+      this.user = {
+        name: 'anshu',
+        emailId: '123@xyz.com',
+        Hash: { number: 10, arrayHash: ['hi', 'bye'] },
+        likes: 10,
+        issuesSolved: 69,
+        issues: 96
+      }
+    }
   }
 
-  return (
-    <nav className={drawerClasses}>
-      <div className='user-profile'>
-        <img className='user-circle-profile' src='' />
-        <div className='user-name-profile'>
-          {props.user.name}
-          <div className='userId'>
-            {props.user.emailId}
+  render () {
+    let drawerClasses = 'side-drawer'
+    if (this.props.show) {
+      drawerClasses = 'side-drawer open'
+    }
+
+    return (
+      <nav className={drawerClasses}>
+        <div className='user-profile'>
+          <img className='user-circle-profile' src=''/*insest photo*/ />
+          <div className='user-name-profile'>
+            {this.user.name}
+            <div className='userId'>
+              {this.user.emailId}
+            </div>
           </div>
         </div>
-      </div>
-      <ul>
-        <li><a href='/home'>Home</a></li>
-      </ul>
-      <div className='hash'>
-        <Hashes numberHash={props.user.Hash.number} arrayHash={props.user.Hash.arrayHash} />
-        <Activity likes={props.user.likes} issuesSolved={props.user.issuesSolved} issues={props.user.issues} />
-      </div>
-      <div className='activity-side' />
-    </nav>
-  )
+        <ul>
+          <li><a href='/home'>Home</a></li>
+        </ul>
+        <div className='hash'>
+          <Hashes numberHash={this.user.Hash.number} arrayHash={this.user.Hash.arrayHash} />
+          <Activity likes={this.user.likes} issuesSolved={this.user.issuesSolved} issues={this.user.issues} />
+        </div>
+        <div className='activity-side' />
+      </nav>
+    )
+  }
 }
 
-export default sideDrawer
+export default SideDrawer
