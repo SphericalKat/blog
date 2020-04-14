5b84e94d260793eeb2bf109d35fa4f0f282251c2
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-14 16:04:27 +0530

    Components made

---
+++ src/components/cardh/indexForum.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Post from './indexForumUser'
+
+class IndexForumCard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.user = props.user
+  }
+
+  render () {
+    return (
+      <div className='indexforum-first'>
+        <div className='indexforum-boxes'>
+          <div className='indexforum-first-header'>
+            Latest Queries
+          </div>
+          <Post user={this.user} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IndexForumCard
